67d5e2e062aaee9462281a2d4cd8436e5144385a
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2023-03-10 16:47:04 +0100

    Add basic style CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        <p>This page was coded by{" "}
+          <a href="https://kristen-n.netlify.app/" target="_blank">Kristen Namigai</a>🧝🏻‍♀️and is{" "}
+          <a href="https://github.com/KrisTenFour/react-weather-app" target="_blank">open-sourced on GitHub</a></p>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
 
+import "./Weather.css"
+
 export default function Weather() {
     return (
         <div className="Weather">
@@ -12,31 +14,30 @@ export default function Weather() {
                         </div>
                         <div className="col-3">
                             <input type="submit" value="Search"
-                                className="btn btn-primary"></input>
+                                className="btn btn-primary w-100"></input>
                         </div>
                     </div>
                 </form>
-                <div className="Date">
-                    <p>Tuesday, 09:00, 10/03/2023</p>
-                </div>
                 <div className="City">
                     <h1>London</h1>
                 </div>
-                <div className="Temperature">
+                <div className="Date subtext">
+                    <p>Tuesday, 09:00, 10/03/2023</p>
+                </div>
+                <div className="Current-temp">
                     <div className="row">
-                        <div className="col-6">
-                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" />
+                        <div className="col d-flex justify-content-start">
+                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" alt="Weather icon" />
+                            <p className="Temperature">10°C</p>
                         </div>
-                        <div className="col-6">
-                            <p>10°C</p>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Light rain</li>
-                                <li>Humidity: 10%</li>
-                                <li>Wind: 5km/h</li>
-                            </ul>
-                        </div>
+                        <div className="col d-flex justify-content-center">
+                            <div className="Card subtext">
+                                <ul>
+                                    <li>Light rain</li>
+                                    <li>Humidity: 10%</li>
+                                    <li>Wind: 5km/h</li>
+                                </ul>
+                            </div></div>
                     </div>
                 </div>
             </div>
