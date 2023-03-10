136ad5b1f5ce6fa169ca1e3df263827bec098e5f
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2023-03-10 16:52:46 +0100

    Add rel = noreferrer to please React

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <Weather />
       <footer>
         <p>This page was coded by{" "}
-          <a href="https://kristen-n.netlify.app/" target="_blank">Kristen Namigai</a>🧝🏻‍♀️and is{" "}
-          <a href="https://github.com/KrisTenFour/react-weather-app" target="_blank">open-sourced on GitHub</a></p>
+          <a href="https://kristen-n.netlify.app/" target="_blank" rel="noreferrer">Kristen Namigai</a>🧝🏻‍♀️and is{" "}
+          <a href="https://github.com/KrisTenFour/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a></p>
       </footer>
     </div>
   );
