1400b0ffb35092c4d82d162c53a435be905c872e
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2023-03-16 16:50:42 +0100

    Update footer (add Netlify link)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <footer>
         <p>This page was coded by{" "}
           <a href="https://kristen-n.netlify.app/" target="_blank" rel="noreferrer">Kristen Namigai</a>🧝🏻‍♀️and is{" "}
-          <a href="https://github.com/KrisTenFour/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a></p>
+          <a href="https://github.com/KrisTenFour/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a> and hosted on {" "}
+          <a href='https://www.netlify.com/' target="_blank" rel="noreferrer">Netlify</a>
+        </p>
       </footer>
     </div>
   );
