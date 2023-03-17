7cd089e13e387c9683e404b4be04eb139cb795b4
KrisTenFour <109226404+KrisTenFour@users.noreply.github.com>
2023-03-17 12:15:58 +0100

    Create and update WeatherForecast CSS file

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,8 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
+import "./WeatherForecast.css"
+
 export default function WeatherForecast() {
     return (
         <div className="WeatherForecast">
@@ -10,7 +12,7 @@ export default function WeatherForecast() {
                     <WeatherIcon iconCode="11d" size={35} />
                     <div className="Forecast-temperature">
                         <span className="Forecast-temp-max">19°</span>
-                        {" "}
+                        |{" "}
                         <span className="Forecast-temp-min">15°</span>
                     </div>
                 </div>
